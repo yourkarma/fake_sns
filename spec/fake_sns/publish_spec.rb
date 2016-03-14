557f233ee95117587cda6bfc71c7240b9ce0e2af
Olof-Joachim Frahm <olof@lshift.net>
2016-03-14 17:16:16 +0000

    Add test case for publishing to target endpoint.

--- spec/fake_sns/publish_spec.rb
+++ spec/fake_sns/publish_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe "Publishing" do
 
   let(:existing_topic) { sns.create_topic(name: "my-topic").topic_arn }
+  let(:existing_platform_endpoint) { sns.create_platform_endpoint(platform_application_arn: "app-arn", token: "token").endpoint_arn }
 
   it "remembers published messages" do
     message_id = sns.publish(topic_arn: existing_topic, message: "hallo").message_id
@@ -21,6 +22,14 @@ RSpec.describe "Publishing" do
     expect {
       sns.publish(topic_arn: existing_topic, message: "A" * 262145)
     }.to raise_error Aws::SNS::Errors::InvalidParameterValue
+  end
+
+  it "allows messages directly to a target ARN" do
+    message_id = sns.publish(target_arn: existing_platform_endpoint, message: "hallo").message_id
+    messages = $fake_sns.data.fetch("messages")
+    expect(messages.size).to eq 1
+    message = messages.first
+    expect(message.fetch(:id)).to eq message_id
   end
 
 end
